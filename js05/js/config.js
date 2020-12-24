1aea232afcd946f7bf827d8d4cfc2b6541dc4c37
pojiko18 <68936496+pojiko18@users.noreply.github.com>
2020-12-25 00:21:10 +0900

    js05/map/gnavi_API

---
+++ js05/js/config.js
@@ -0,0 +1,12 @@
+
+var firebaseConfig = {
+    apiKey: "AIzaSyDf4oZYiwigP1f08-ZH8TnWn5jSdJSon48",
+    authDomain: "kanjisan-94617.firebaseapp.com",
+    databaseURL: "https://kanjisan-94617.firebaseio.com",
+    projectId: "kanjisan-94617",
+    storageBucket: "kanjisan-94617.appspot.com",
+    messagingSenderId: "965629351918",
+    appId: "1:965629351918:web:f3bdf546fa9dd1eea1a0ff"
+};
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
